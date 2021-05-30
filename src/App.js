8c41322edbe6c383c429c3f2acd20afd27b78c91
Nermin Mitkov <nermin.mitkov1998@gmail.com>
2021-05-31 00:55:30 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  
+  //state
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState ([]);
+  const [status, setStatus] = useState ('all');
+  const [filteredTodos, setFilteredTodos] = useState ([]);
+
+  //single time use
+  useEffect(() =>{
+    getLocaltodos();
+  }, []);
+
+  //use effect
+  useEffect(() => {
+      //functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+        
+      };
+    };
+    
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  //Save to local
+  const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getLocaltodos = () => {
+    if (localStorage.getItem("todos") === null ) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos", JSON.stringify(todos)));
+      setTodos(todoLocal);
+    }
+
+  };
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Nermo's todo list!</h1>
       </header>
+      <Form inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText} 
+      setStatus={setStatus} 
+      
+      />
+      <TodoList setTodos={setTodos} filteredTodos={filteredTodos} todos = {todos} />
     </div>
   );
 }
